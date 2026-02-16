3d69109e80bb672f366be259f20627dc52f3a586
Theodore Harlan <hpt@bu.edu>
2026-02-15 20:38:53 -0500

    test

--- mini_insta/urls.py
+++ mini_insta/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.ProfileListView.as_view(), name="show_all_profiles"),
+    path('mini_insta/profile/<int:pk>/', views.ProfileDetailView.as_view(), name='show_profile'),
 ]
--- mini_insta/views.py
+++ mini_insta/views.py
@@ -2,9 +2,16 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views.generic import ListView
+from django.views.generic import DetailView
 from .models import Profile
 
 class ProfileListView(ListView):
     model = Profile
     template_name = "show_all_profiles.html"
     context_object_name = "profiles"
+
+
+class ProfileDetailView(DetailView):
+    model = Profile
+    template_name = "show_profile.html"
+    context_object_name = "profile"
